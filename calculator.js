5895ec610f59518c82093cc4d3f31cd28d03b172
Vedika9 <147727998+Vedika9@users.noreply.github.com>
2023-10-14 21:20:25 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,25 @@
+let display = document.getElementById('display');
+let currentInput = '';
+
+function appendToDisplay(value) {
+    currentInput += value;
+    display.value = currentInput;
+}
+
+function clearDisplay() {
+    currentInput = '';
+    display.value = '';
+}
+
+function calculateResult() {
+    try {
+        const result = eval(currentInput);
+        if (isNaN(result)) {
+            display.value = 'Error';
+        } else {
+            display.value = result;
+        }
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
